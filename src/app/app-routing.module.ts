71a66b50f9a7c6e037129144a9b62dcf9e79044a
Franklin Yu <franklinyu@hotmail.com>
2017-07-04 15:05:42 -0400

    Generate OAuth component
    Add styles to active router link.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ItemListComponent } from 'app/item-list/item-list.component'
+import { OauthComponent } from 'app/oauth/oauth.component'
 
 const routes: Routes = [
   {
     path: 'items',
     component: ItemListComponent
+  }, {
+    path: 'oauth',
+    component: OauthComponent
   }, {
     path: '',
     redirectTo: '/items',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from 'app/app-routing.module';
 import { AppComponent } from 'app/app.component';
 import { ItemListComponent } from 'app/item-list/item-list.component';
 import { ItemService } from 'app/services/item.service';
+import { OauthComponent } from 'app/oauth/oauth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent
+    ItemListComponent,
+    OauthComponent
   ],
   imports: [
     BrowserModule,
